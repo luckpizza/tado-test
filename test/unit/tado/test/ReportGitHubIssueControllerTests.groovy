7fdd6aa4b2d5fecea58da342c2f89e96d7ec945a
Guest <guest@localhost.havel>
2013-02-26 16:38:24 +0100

    adding new files and test directories

---
+++ test/unit/tado/test/ReportGitHubIssueControllerTests.groovy
@@ -0,0 +1,17 @@
+package tado.test
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ReportGitHubIssueController)
+class ReportGitHubIssueControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
